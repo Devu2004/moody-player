570c8e240891d28835125bf5d7a70e91c5a4868c
Devu2004 <devansh7595@gmail.com>
2025-10-09 17:41:56 +0530

    testing

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const Songsrouter = require('./routes/Song.routes')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const Songsrouter = require('./routes/Song.routes');
 
-const server = express()
+const server = express();
 
-server.use(cors())
-server.use(express.json())
-server.use('/',Songsrouter)
+server.use(cors());
+server.use(express.json());
+
+server.use('/songs', Songsrouter);
+
+server.get('/', (req, res) => {
+  res.send('Moody Player Backend Running');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
 module.exports = server
---
+++ backend/src/dist/app.dev.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var express = require('express');
+
+var cors = require('cors');
+
+var Songsrouter = require('./routes/Song.routes');
+
+var server = express();
+server.use(cors());
+server.use(express.json());
+server.use('/songs', Songsrouter);
+server.get('/', function (req, res) {
+  res.send('Moody Player Backend Running');
+});
+var PORT = process.env.PORT || 3000;
+server.listen(PORT, function () {
+  console.log("Server running on port ".concat(PORT));
+});
+module.exports = server;
