af18aaf9870b9d0779384d6ebaa0453e1cab7f66
Devu2004 <devansh7595@gmail.com>
2025-10-09 21:19:31 +0530

    testing ongoing

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function connectToDB() {
-  mongoose.connect(process.env.MONGODB_URL).then(()=>{
+  mongoose.connect(process.env.MONGODB_URL,{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log('Connected to MongoDb');
   })
   .catch((err)=>{
--- backend/src/db/dist/db.dev.js
+++ backend/src/db/dist/db.dev.js
@@ -3,7 +3,10 @@
 var mongoose = require("mongoose");
 
 function connectToDB() {
-  mongoose.connect(process.env.MONGODB_URL).then(function () {
+  mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(function () {
     console.log('Connected to MongoDb');
   })["catch"](function (err) {
     console.log('Error: ', err);
